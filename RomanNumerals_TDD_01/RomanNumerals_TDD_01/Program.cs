c9cb10f54568283f315c8a729eb186be691e92c8
DavidChicks <dchicks0@hotmail.com>
2016-09-14 11:55:44 -0700

    Iniial test

---
+++ RomanNumerals_TDD_01/RomanNumerals_TDD_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals_TDD_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // parse each arg and print decimal value
+        }
+
+    }
+
+    public class RomanNumerals
+    {
+        public static int ParseRomanNumeral(string romanNumeral)
+        {
+            return 0;
+        }
+    }
+}
---
+++ RomanNumerals_TDD_01/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals_TDD_01;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_I()
+        {
+            int value = RomanNumerals.ParseRomanNumeral("I");
+            Assert.AreEqual(1, value);
+        }
+    }
+}
